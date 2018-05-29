4f4125ed10d7a5964ec911417b0358fcfd8887bd
CoderDade <coder.dade@gmail.com>
2018-05-29 12:00:52 +0800

    remove useless code

--- mybin/src/test/java/com/dade/bin/test/BINTestEntity.java
+++ mybin/src/test/java/com/dade/bin/test/BINTestEntity.java
@@ -10,22 +10,6 @@ public class BINTestEntity {
     @Order(2)
     String str;
 
-    public Integer getLen() {
-        return len;
-    }
-
-    public void setLen(Integer len) {
-        this.len = len;
-    }
-
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
-
     @Override
     public String toString() {
         return "BINTestEntity{" +
